9c976f2f9460883d9f6812c1a32c60e3c72c4eca
Simon Wiering <simon.wiering@hotmail.com>
2021-05-26 20:53:30 +0200

    user --18

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,40 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Hi <b>{{ Auth::user()->name }}</b>
+
+            <span style="float:right ">
+                Total Users: <span class="badge badge-danger">{{ count($users) }} </span>
+            </span>
         </h2>
     </x-slot>
 
-    <!-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+    <div class="py-12">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Srl. Nr.</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Created At</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $single_user)
+                                <tr>
+                                    <th scope="row">{{ $single_user->id }}</th>
+                                    <td>{{ $single_user->name }}</td>
+                                    <td>{{ $single_user->email }}</td>
+                                    <td>{{ $single_user->created_at->diffForHumans() }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div> -->
+    </div>
 </x-app-layout>

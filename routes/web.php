fcd5ea14e76fd7cb83c2b3a441c6a9e5f02e76a2
Simon Wiering <simon.wiering@hotmail.com>
2021-05-31 10:52:49 +0200

    email verification

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,11 @@ Route::get('/brand/delete/{id}', [BrandController::class, 'Delete']);
 Route::get('/multi/image', [BrandController::class, 'Multipic'])->name('multi.image');
 Route::post('/multi/add', [BrandController::class, 'StoreImage'])->name('store.image');
 
+// Email verification
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
 
 Route::get('/contact', [ContactController::class, 'index'])->name('con');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
